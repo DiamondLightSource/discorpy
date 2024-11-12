53fec84dc2ea7c185baecfe0e147ed6df5546508
nghia-vo <nvo@bnl.gov>
2024-11-12 18:06:22 -0500

    Update proc module

--- discorpy/proc/processing.py
+++ discorpy/proc/processing.py
@@ -780,11 +780,11 @@ def _find_cross_point_between_parabolas(para_coef_hor, para_coef_ver):
     """
     a1, b1, c1 = para_coef_hor[0:3]
     a2, b2, c2 = para_coef_ver[0:3]
-    xvals = np.float32(np.real(
-        np.roots([a1 ** 2 * a2, 2 * a1 * a2 * b1,
-                  a2 * b1 ** 2 + a1 * b2 + 2 * a1 * a2 * c1,
-                  -1 + b1 * b2 + 2 * a2 * b1 * c1,
-                  b2 * c1 + a2 * c1 ** 2 + c2])))
+    xvals = np.real_if_close(np.real(np.roots(
+        [a1 ** 2 * a2, 2 * a1 * a2 * b1,
+         a2 * b1 ** 2 + a1 * b2 + 2 * a1 * a2 * c1,
+         -1 + b1 * b2 + 2 * a2 * b1 * c1,
+         b2 * c1 + a2 * c1 ** 2 + c2])))
     if len(xvals) == 0:
         raise ValueError("Can't find a cross point between two parabolas")
     if len(xvals) > 1:
